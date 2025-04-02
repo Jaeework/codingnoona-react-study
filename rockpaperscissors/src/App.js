ef61c1a10c0d8b90f86f2de44e023b0640a01cf9
Jaeework <jaeework@gmail.com>
2025-04-02 15:39:51 +0900

    feat: Add game result logic and color feedback
    - 유저와 컴퓨터의 선택을 비교하여 승패 결과 판정 기능 구현
    - 결과에 따라 테두리 색상 변경

--- rockpaperscissors/src/App.js
+++ rockpaperscissors/src/App.js
@@ -26,12 +26,36 @@ const choice = {
 function App() {
   const [userSelect, setUserSelect] = useState(null)
   const [computerSelect, setComputerSelect] = useState(null)
+  const [result, setResult] = useState("");
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
     let computerChoice = randomChoice();
     setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
   };
+
+  const judgement = (user, computer) => {
+    console.log('user: ', user, 'computer : ', computer);
+
+    // user === computer tie
+    // user === rock, computer === "scissors"  user win
+    // user === rock, computer === "paper"  user lose
+    // user === scissors, computer === paper  user win
+    // user === scissors, computer === rock  user lose
+    // user === paper, computer === rock  user win
+    // user === paper, computer == scissors  user lose
+    if(user.name === computer.name) {
+      return "tie"
+    } else if(user.name === "Rock") {
+      return computer.name === "Scissors" ? "win" : "lose";
+    } else if(user.name === "Scissors") {
+      return computer.name === "Paper" ? "win" : "lose";
+    } else if(user.name === "Paper") {
+      return computer.name === "Rock" ? "win" : "lose";
+    }
+
+  }
 
   const randomChoice = () => {
     let itemArray = Object.keys(choice); // 객체의 키 값만 뽑아서 배열로 만들어주는 함수
@@ -43,8 +67,8 @@ function App() {
   return (
     <div>
       <div className="main">
-        <Box title="You" item={userSelect}/>
-        <Box title="Computer" item={computerSelect} />  
+        <Box title="You" item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect} result={result}/>  
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
--- rockpaperscissors/src/component/Box.js
+++ rockpaperscissors/src/component/Box.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
 const Box = (props) => {
+  let result;
+  if(props.title === "Computer" && props.result && props.result !== "tie") {
+    result = props.result === "win" ? "lose" : "win";
+  } else {result = props.result}
+
   return (
-    <div className="box">
+    <div className={`box ${result}`}>
         <h1>{props.title}</h1>
         <img className="item-img" 
             src={props.item && props.item.img} alt={props.item && props.item.name} />
-        <h2>WIN</h2>
+        <h2>{result}</h2>
     </div>
   )
 }
