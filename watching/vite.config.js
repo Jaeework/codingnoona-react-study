73e5d24e4c27e2083bdb2b05df4b8b39c2c1d3d9
Jaeework <jaeework@gmail.com>
2025-04-19 23:47:40 +0900

    style: fix Banner height for mobile screen
    - 모바일 화면에서의 배너 높이 조절
    - vite.config.js 파일 원복

--- watching/vite.config.js
+++ watching/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    plugins: [react()],
-    define: {
-      'import.meta.env.VITE_APP_API_KEY': JSON.stringify(env.REACT_APP_API_KEY || env.VITE_APP_API_KEY),
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
